43ba3bbc99ce9bb086d5715b3efe2857d707acf0
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-20 19:48:26 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,7 +7,7 @@ def position_taken? (board, index)
   end
 
 def valid_move?(board, index)
-  if index.between?(1,9) && !position_taken?(board, index)
+  if index.between?(0,8) && !position_taken?(board, index)
     true
   end
 end
